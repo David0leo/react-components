8ad913b700a49a83479b0640d3903935dc9b0b3b
David0leo <dla2@ualberta.ca>
2017-12-31 15:36:42 -0700

    Create card.js

---
+++ card/card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const defaultStyle = {
+	width: "100%",
+	height: "100%",
+	borderRadius: "0.5em",
+	border: "1px solid rgba(0, 0, 0, 0.1)",
+	boxShadow: "0 0 15px 1px rgba(0, 0, 0, 0.25)",
+	overflow: "hidden"
+};
+
+const Card = ({ style, children }) => (
+	<div style={Object.assign({}, defaultStyle, style)}>{children}</div>
+);
+
+export default Card;
