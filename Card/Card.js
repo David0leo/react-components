eadef9c0c7874c8c13c47bf988aa65eb6f11e93f
David0leo <dla2@ualberta.ca>
2018-01-11 21:18:43 -0700

    Make Card a styled component.

--- Card/Card.js
+++ Card/Card.js
@@ -1,16 +1,13 @@
 import React from "react";
+import styled from 'styled-components';
 
-const defaultStyle = {
-	width: "100%",
-	height: "100%",
-	borderRadius: "0.5em",
-	// border: "1px solid rgba(0, 0, 0, 0.15)",
-	boxShadow: "0 0 15px 1px rgba(0, 0, 0, 0.25)",
-	overflow: "hidden"
-};
-
-const Card = ({ style, children }) => (
-	<div style={Object.assign({}, defaultStyle, style)}>{children}</div>
-);
+const Card = styled.div`
+	width: 100%;
+	height: 100%;
+	border-radius: 0.5em;
+	border: 1px solid rgba(0, 0, 0, 0.1);
+	box-shadow: 0 0 15px 1px rgba(0, 0, 0, 0.25);
+	overflow: hidden;
+`;
 
 export default Card;
